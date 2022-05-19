f7411a51af2b51bebcd961ccd9b877d0ff485852
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2022-05-19 18:07:09 -0500

    added density function

---
+++ R/make_cdd_density_tbl.R
@@ -0,0 +1,15 @@
+#' Manually generate kernel density estimates for a range of CDD values, truncated to 0 CDD and the max CDD value in the input data.
+#'
+#' @param data: data.frame with a cdd column
+#'
+#' @return A data.frame with a sequence of CDD values and density estimates, suitable for plotting with geom_line()
+#' @export
+#' @import dplyr
+#' @importFrom magrittr %>%
+#'
+make_cdd_density_tbl <- function(data){
+
+  dens <- density(data[["cdd"]], na.rm = T)
+  tibble(cdd = dens[["x"]], dens = dens[["y"]]) %>%
+    filter(cdd > 0, cdd < max(data[["cdd"]], na.rm = T))
+}
