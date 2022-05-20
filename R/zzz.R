158cd6d73412a93378a21170fc4a32872c9d0c58
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2022-05-20 11:51:35 -0500

    adding font loading for theme consistency

---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+#' @import sysfonts showtext
+.onLoad <- function(libname, pkgname){
+  sysfonts::font_add_google("Lexend Deca", "Lexend Deca SemiBold", regular.wt = 600)
+  sysfonts::font_add_google("Lexend Deca", "Lexend Deca Thin", regular.wt = 100)
+  sysfonts::font_add_google("Lexend Deca", "Lexend Deca Light", regular.wt = 300)
+  sysfonts::font_add_google("Roboto Condensed", "Roboto Condensed")
+  showtext::showtext_auto()
+}
+
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage("Necessary Google fonts added to tempdir(), and showtext_auto() enabled in order to use them.\nRun showtext::showtext_auto(enable = F) to turn off.\n\nNote that showtext and RStudio plot Viewer do not always play nicely.\nTry saving a plot with grDevices::jpeg to see what text will look like.")
+}
